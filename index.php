436e4b8955202db2772c46d70b65b4c2966e79f3
calvinmeek <kevin1997>
2018-04-03 13:53:29 -0400

    t

--- index.php
+++ index.php
@@ -3,13 +3,7 @@ $servername = "web0.site.uottawa.ca:15432";
 $username = "cmeek070";
 $password = "R964nsm4xt";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
 echo "Connected successfully";
 echo"test";
 ?>
