d5bd8404b1a87f67e3d416c6ac8543a28d1d5f56
calvinmeek <kevin1997>
2018-04-03 14:31:31 -0400

    r

--- index.php
+++ index.php
@@ -18,9 +18,11 @@
 	    echo 'Connected';
 	}
 
-	$sql = pg_query($conn, "CREATE TABLE rater ( INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(15) NOT NULL, lname VARCHAR(20) NOT NULL, email VARCHAR(30) NOT NULL, joinDate DATE NOT NULL, type VARCHAR(15) NOT NULL, rep INT(4) NOT NULL)") ;
+	$sql = pg_query($conn, "CREATE TABLE rater (id INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(15) NOT NULL, lname VARCHAR(20) NOT NULL, email VARCHAR(30) NOT NULL, joinDate DATE NOT NULL, type VARCHAR(15) NOT NULL, rep INT(4) NOT NULL)") ;
+
+	$t1 = pg_query($conn, "INSERT INTO rater(id,fname,lname,email,joinDate,type,rep) VALUES ('Calvin', 'Meek', 'cmeek070@uottawa.ca','Student',7)")
 	
-	$result = pg_query($conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+	$result = pg_query($conn, "SELECT * FROM rater");
 
 	print "<pre>\n";
 
