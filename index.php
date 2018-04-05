76684277565a7709a076655e5dd6525dda34a109
calvinmeek <kevin1997>
2018-04-05 13:07:19 -0400

    r

--- index.php
+++ index.php
@@ -28,65 +28,27 @@
 
 	$rater = pg_query($conn, "CREATE TABLE IF NOT EXISTS rater(id SERIAL PRIMARY KEY, email VARCHAR(30) NOT NULL, name VARCHAR(15) NOT NULL, join_date VARCHAR(30) NOT NULL, type VARCHAR(15) NOT NULL, rep INT NOT NULL)");
 
-	// if (!$rater) {
-	//   echo "Creating rater is not working. \n";
-	//   exit;
-	// }
-	// else{
-	//   echo "Rater Table exists\n";
-	// }
 
 
 
 	$restaraunt = pg_query($conn, "CREATE TABLE IF NOT EXISTS restaraunt(restarauntID SERIAL PRIMARY KEY, name VARCHAR(25) NOT NULL, type VARCHAR(25) NOT NULL, url VARCHAR(255) NOT NULL)");
 
-	// if (!$restaraunt) {
-	//   echo "Creating restaraunt is not working. \n";
-	//   exit;
-	// }
-	// else{
-	//   echo "restaraunt Table exists\n";
-	// }
-
 
 
 	$rating = pg_query($conn, "CREATE TABLE IF NOT EXISTS rating(id INT REFERENCES rater(id), rating_date DATE PRIMARY KEY, price INT, food VARCHAR(25), 			mood VARCHAR(25), commments VARCHAR(255), restarauntID INT REFERENCES restaraunt(restarauntID))");
 
-	// if (!$rating) {
-	//   echo "Creating rating is not working. \n";
-	//   exit;
-	// }
-	// else{
-	//   echo "Rating Table exists\n";
-	// }
-
-
-	
 
 
 	$location = pg_query($conn, "CREATE TABLE IF NOT EXISTS location(locationID SERIAL PRIMARY KEY, first_open_date DATE NOT NULL, manager_name VARCHAR(25) NOT NULL,	phoneNumber VARCHAR(15) NOT NULL, address VARCHAR(255) NOT NULL, open_hour INT NOT NULL, close_hour INT NOT NULL, restarauntID INT REFERENCES restaraunt(restarauntID))");
 
-	// if (!$location) {
-	//   echo "Creating location is not working. \n";
-	//   exit;
-	// }
-	// else{
-	//   echo "location Table exists\n";
-	// }
 
 
 	$menuItem = pg_query($conn, "CREATE TABLE IF NOT EXISTS menuItem(itemID INT PRIMARY KEY NOT NULL, name VARCHAR(25) NOT NULL, type VARCHAR(25) NOT NULL, category VARCHAR(25) NOT NULL, description VARCHAR(255) NOT NULL, price INT NOT NULL, restarauntID INT REFERENCES restaraunt(restarauntID))");
 
-	// if (!$menuItem) {
-	//   echo "Creating menuItem is not working. \n";
-	//   exit;
-	// }
-	// else{
-	//   echo "menuItem Table exists\n";
-	// }
 
 
 	$ratingItem = pg_query($conn, "CREATE TABLE IF NOT EXISTS ratingItem(id INT REFERENCES rater(id), date_stamp VARCHAR(25) NOT NULL, itemID INT REFERENCES menuItem(itemID) NOT NULL, rating INT NOT NULL, comment VARCHAR(255), PRIMARY KEY(id,date_stamp,itemID))");
+
 
 	if (!$ratingItem && !$menuItem && !$location && !$rating && !$restaraunt && !$rater) {
 	  echo "TABLE_GENERATION_FAILED\n";
@@ -103,6 +65,7 @@
 
 	// ############################################################# POPULATING TABLES #############################################################
 
+
 	// $$$$$$$$$$$$$$$$$$$$$$ ADDING RATERS $$$$$$$$$$$$$$$$$$$$$$
 
 	$rater0 = pg_query($conn, "INSERT INTO rater(email,name,join_date,type,rep) VALUES ('cmeek070@email.ca','Calvin','05-06-99','Blog',5)");
@@ -116,12 +79,33 @@
 	$rater8 = pg_query($conn, "INSERT INTO rater(email,name,join_date,type,rep) VALUES ('m1234566@email.ca','Mike','11-17-99','Blog',1)");
 	$rater9 = pg_query($conn, "INSERT INTO rater(email,name,join_date,type,rep) VALUES ('kyle123@email.ca','Kyle','09-21-96','Blog',1)");
 	$rater10 = pg_query($conn, "INSERT INTO rater(email,name,join_date,type,rep) VALUES ('oaddddddd@email.ca','Owen','08-12-95','Online',4)");
-	$rater11= pg_query($conn, "INSERT INTO rater(email,name,join_date,type,rep) VALUES ('noahman@email.ca','Noah','12-22-12','Blog',3)");
+	$rater11 = pg_query($conn, "INSERT INTO rater(email,name,join_date,type,rep) VALUES ('noahman@email.ca','Noah','12-22-12','Blog',3)");
 	$rater12 = pg_query($conn, "INSERT INTO rater(email,name,join_date,type,rep) VALUES ('baseball123@email.ca','Ryan','10-20-30','Critic',4)");
 	$rater13 = pg_query($conn, "INSERT INTO rater(email,name,join_date,type,rep) VALUES ('gamerguy1@email.ca','David','05-30-12','Critic',4)");
 	$rater14 = pg_query($conn, "INSERT INTO rater(email,name,join_date,type,rep) VALUES ('biker1234578@email.ca','Jake','11-12-23','Online',5)");
 	
+
 	// $$$$$$$$$$$$$$$$$$$$$$ ADDING RESTRAUNTS $$$$$$$$$$$$$$$$$$$$$$
+
+	$restraunt0 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Best Food Ever','American','www.bestfood.com')");
+	$restraunt1 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Pizza Lovers','American','www.pizzalovers.com')");
+	$restraunt2 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Sushi Palace','Sushi','www.sushipalace.com')");
+	$restraunt3 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Italian Pasta','Italian','www.ipasta.com')");
+	$restraunt4 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Burger Master','American','www.burgermaster.com')");
+	$restraunt5 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Icecream Delight','Dessert','www.id.com')");
+	$restraunt6 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Taste of Asia','Asian','www.toa.com')");
+	$restraunt7 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Steak House','American','www.steakhouse.com')");
+	$restraunt8 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Shawarma Palooza','Middle Eastern','www.spalooza.com')");
+	$restraunt9 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Eat 'n Play,'American','www.eatnplay.com')");
+	$restraunt10 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('Ballin Burgers','American','www.ballinburgers.com')");
+	$restraunt11 = pg_query($conn, "INSERT INTO restaraunt(name,type,url) VALUES ('French Cuisine','French','www.fc.com')");
+
+
+
+
+
+
+
 
 
 
@@ -154,12 +138,43 @@
 	  echo "NO RECORDS FOUND";
 	}
 
-	// if (!pg_num_rows($result)) {
-	//   print("Connection is working, but database is empty.\n");
-	// } else {
-	//   print "Tables in your database:\n";
-	//   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-	// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ?>
