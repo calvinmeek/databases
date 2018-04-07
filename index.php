2db76d76b1c337105a5a6a512a8d2acfdee1aea0
calvinmeek <kevin1997>
2018-04-07 14:57:44 -0400

    r

--- index.php
+++ index.php
@@ -399,7 +399,7 @@
 
 	$testString = "Ballin Burgers";
 
-	$queryB = pg_query($conn, "SELECT id, name FROM menuItem, restaurant WHERE menuItem.restaurantID = restaurant.restaurantID AND restaurant.name = '$testString'");
+	$queryB = pg_query($conn, "SELECT * FROM menuItem, restaurant WHERE menuItem.restaurantID = restaurant.restaurantID AND restaurant.name = '$testString'");
 
 
 	print "<pre>\n";
