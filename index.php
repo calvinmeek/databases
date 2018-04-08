9237073cb2034e301b11ad07e3be2f2c5460f9ee
calvinmeek <kevin1997>
2018-04-08 18:03:04 -0400

    r

--- index.php
+++ index.php
@@ -889,7 +889,7 @@
 	// %%%%%%%%%%%%%%%%% QUERY K %%%%%%%%%%%%%%%%%
 
 
-	$queryK = pg_query($conn, "SELECT name, join_date FROM rater r INNER JOIN (SELECT id, MAX(food) AS Max_Food, MAX(mood) AS Max_Mood FROM rating GROUP BY id) rtng ON r.id = rtng.id AND rtng.food = Max_Food AND rtng.mood = Max_Mood");
+	$queryK = pg_query($conn, "SELECT name, join_date FROM rater r INNER JOIN (SELECT id, MAX(food) AS Max_Food, MAX(mood) AS Max_Mood FROM rating GROUP BY id) rtng ON r.id = rtng.id AND r.food = rtng.Max_Food AND r.mood = rtng.Max_Mood");
 
 	print "<pre>\n";
 	print "QUERY K\n\n";
