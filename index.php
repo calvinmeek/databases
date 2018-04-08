f7a1dc70bfe297b9e5d3bad4556efc180dd035d0
calvinmeek <kevin1997>
2018-04-08 17:43:41 -0400

    r

--- index.php
+++ index.php
@@ -848,7 +848,7 @@
 
 	$testString6 = "Asian";
 
-	$queryJ = pg_query($conn, "SELECT * FROM restaurant WHERE restaurantID IN (SELECT R.name, R.restaurantID, Rt.price, Rt.food, Rt.mood, Rt.staff FROM restaurant R, rating Rt GROUP BY R.restaurantID, R.name, Rt.price, Rt.food, Rt.mood, Rt.staff HAVING Rt.price + Rt.food + Rt.mood + Rt.staff > 15 AND )");
+	$queryJ = pg_query($conn, "SELECT * FROM restaurant WHERE restaurantID IN (SELECT R.name, R.restaurantID, Rt.price, Rt.food, Rt.mood, Rt.staff FROM restaurant R, rating Rt GROUP BY R.restaurantID, R.name, Rt.price, Rt.food, Rt.mood, Rt.staff HAVING Rt.price + Rt.food + Rt.mood + Rt.staff > 15)");
 
 	print "<pre>\n";
 	print "QUERY J\n\n";
